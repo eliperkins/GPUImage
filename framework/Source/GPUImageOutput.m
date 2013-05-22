0828e32871ae9b4a8b1d2651954761785c2d8293
eliperkins <eli@onemightyroar.com>
2013-05-22 16:42:18 -0300

    Changed call from imageWithCGImage:scale:orientation: to imageWithCGImage:
    Was causing the image to be oriented the wrong way.

--- framework/Source/GPUImageOutput.m
+++ framework/Source/GPUImageOutput.m
@@ -362,7 +362,7 @@ void reportAvailableMemoryForGPUImage(NSString *tag)
 - (UIImage *)imageFromCurrentlyProcessedOutputWithOrientation:(UIImageOrientation)imageOrientation;
 {
     CGImageRef cgImageFromBytes = [self newCGImageFromCurrentlyProcessedOutputWithOrientation:imageOrientation];
-    UIImage *finalImage = [UIImage imageWithCGImage:cgImageFromBytes scale:1.0 orientation:imageOrientation];
+    UIImage *finalImage = [UIImage imageWithCGImage:cgImageFromBytes];
     CGImageRelease(cgImageFromBytes);
     
     return finalImage;
